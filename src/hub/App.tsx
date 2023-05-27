5dd9ae80f292c5f8b5654accfa7c7f17c6ef71f9
Wojciech Wesolowski <wojciech.wesolowski@avanade.com>
2023-05-27 22:51:52 +0200

    Add a checkbox Personal to enable saving a personal notes

--- src/hub/App.tsx
+++ src/hub/App.tsx
@@ -26,7 +26,17 @@ const App = () => {
     const workItemId = React.useRef(0);
     const [storageInputValue, setStorageInputValue] = React.useState("");
 
+    const [checked, setChecked] = React.useState(false);
+    const personal = React.useRef(false);
+    const userId = React.useRef<string | null>(null);
+
     console.log("render");
+
+    const onCheckHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        personal.current = !personal.current;
+        setChecked(personal.current);
+        loadData();
+    }
 
     const getWorkItemFormService = async() => {
         return await SDK.getService<IWorkItemFormService>(WorkItemTrackingServiceIds.WorkItemFormService);
@@ -77,10 +87,11 @@ const App = () => {
         let document: IData;
 
         const witId = workItemId.current;
+        const id = personal.current ? `${userId.current}.${workItemId.current.toString()}` : workItemId.current.toString();
 
         try
         {
-            document = await dataManager.getDocument("myExt", workItemId.current.toString());
+            document = await dataManager.getDocument("myExt", id);
             document.data = newData;
 
             console.log("Editing existing storage ...");
@@ -88,7 +99,7 @@ const App = () => {
         } catch {
             console.log("Creating a new storage ...");
             const newDocument: IData = {
-                id: workItemId.current.toString(),
+                id: id,
                 data: newData
             }
             dataManager.createDocument("myExt", newDocument);
@@ -107,14 +118,16 @@ const App = () => {
 
     const loadData = async () => {
         const dataManager = await getDataManager();
+        const id = personal.current ? `${userId.current}.${workItemId.current.toString()}` : workItemId.current.toString();
 
         try
         {
-            const document: IData = await dataManager.getDocument("myExt", workItemId.current.toString());
+            const document: IData = await dataManager.getDocument("myExt", id);
 
             setStorageInputValue(document.data);
         } catch {
-            console.log(`Cannot get a document. Collection 'myExt', Work Item ID: '${workItemId.current}'`);
+            console.log(`Cannot get a document. Collection 'myExt', Work Item ID: '${id}'`);
+            setStorageInputValue("");
         }
     }
     
@@ -122,7 +135,11 @@ const App = () => {
         SDK.register(SDK.getContributionId(), () => {
             return {
                 onLoaded: async() => {                    
-                    maxWordCount.current = SDK.getConfiguration().witInputs["MaxWordCount"];                    
+                    maxWordCount.current = SDK.getConfiguration().witInputs["MaxWordCount"]; 
+                    
+                    const user = SDK.getUser();
+                    userId.current = user.id;
+                    console.log(user);
 
                     fieldName.current = SDK.getConfiguration().witInputs["FieldName"];
 
@@ -162,7 +179,9 @@ const App = () => {
             <div>
                 <label htmlFor='storage'>My Notes:</label><br />
                 <textarea id='storage' onChange={onChangeHandler} value={storageInputValue} /><br />
-                <input type='button' onClick={onSave} value='Save' />
+                <label htmlFor='personal'>Personal</label>
+                <input id='personal' type='checkbox' checked={personal.current} onChange={onCheckHandler} /><br />
+                <input type='button' onClick={onSave} value='Save' />                
             </div>
         </>
     );
